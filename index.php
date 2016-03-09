e6b081bdb32400a6e28d1b804185ce1156f28c69
Domagoj Lipusic <domagoj.lipusic0@gmail.com>
2016-03-09 13:35:22 +0100

    Prvi commit

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 <html>
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8">
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
+
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('Super, jos cemo dodati<br>');
+
+    echo(date('d.m.Y.'));
+
+    $ocjena = 5;
+    echo('<br>Vaša ocjena je: ' . $ocjena);
+    $stanje = 'strašno';
+    echo('<br>Stanje je: '.$stanje);
+?>
 
 <p>
     Ovo je odlomak
